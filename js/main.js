60ca2dbc30f9185b45dbe4ce24d3520f8240ebda
laithsobuh <38588487+laithsobuh@users.noreply.github.com>
2021-04-01 20:05:03 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,103 @@
+$(document).ready(function() {
+
+	
+	/* Navigation burger onclick side navigation show */
+	$('.burger-container').on('click', function() {
+		$('.main-navigation').toggle('slow');
+
+		if($('#myBtn').hasClass('change')) {
+			$('body').addClass('stop-scroll');
+		} else {
+			$('body').removeClass('stop-scroll');
+		}
+	});
+
+
+	/* About me slider */
+	$('.about-me-slider').slick({
+		slidesToShow: 1,
+		prevArrow: '<span class="span-arrow slick-prev"><</span>',
+		nextArrow: '<span class="span-arrow slick-next">></span>'
+	});
+
+	/* Blog slider */
+	$('.blog-slider').slick({
+		slidesToShow: 2,
+		prevArrow: '<span class="span-arrow slick-prev"><</span>',
+		nextArrow: '<span class="span-arrow slick-next">></span>',
+		responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+		]
+	});
+	
+});
+
+
+
+var counta = 0;
+
+$(window).scroll(function(e){
+
+
+	/* Onscroll number counter */
+	var statisticNumbers = $('.single-count');
+	if(statisticNumbers.length) {
+		var oTop = statisticNumbers.offset().top - window.innerHeight;
+		if (counta == 0 && $(window).scrollTop() > oTop) {
+			$('.count').each(function() {
+				var $this = $(this),
+				countTo = $this.attr('data-count');
+				$({
+					countNum: $this.text()
+				}).animate({
+					countNum: countTo
+				},
+
+				{
+					duration: 2000,
+					easing: 'swing',
+					step: function() {
+						$this.text(Math.floor(this.countNum));
+					},
+					complete: function() {
+						$this.text(this.countNum);
+					}
+				});
+			});
+			counta = 1;
+		}
+	}
+
+	
+});
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} ;
